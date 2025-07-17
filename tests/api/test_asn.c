d33b1216b9ea09161fbf69defc1f628b36d424c3
Josh Holtrop <josh@wolfssl.com>
2025-07-17 15:45:16 -0400

    Add test for INTEGER too large for sword32

--- tests/api/test_asn.c
+++ tests/api/test_asn.c
@@ -24,7 +24,7 @@
 #include <tests/api/test_asn.h>
 
 #ifndef NO_ASN
-static int test_SetShortInt_once(sword32 val, byte* valDer, word32 valDerSz)
+static int test_GetSetShortInt_once(sword32 val, byte* valDer, word32 valDerSz)
 {
     EXPECT_DECLS;
 
@@ -61,7 +61,7 @@ static int test_SetShortInt_once(sword32 val, byte* valDer, word32 valDerSz)
 }
 #endif
 
-int test_SetShortInt(void)
+int test_GetSetShortInt(void)
 {
     EXPECT_DECLS;
 
@@ -72,50 +72,50 @@ int test_SetShortInt(void)
     {
         /* Input 1 byte min */
         valDer[0] = 0x00;
-        EXPECT_TEST(test_SetShortInt_once(0x00, valDer, 1));
+        EXPECT_TEST(test_GetSetShortInt_once(0x00, valDer, 1));
 
         /* Input 1 byte max */
         valDer[0] = 0x00;
         valDer[1] = 0xff;
-        EXPECT_TEST(test_SetShortInt_once(0xff, valDer, 2));
+        EXPECT_TEST(test_GetSetShortInt_once(0xff, valDer, 2));
 
         /* Input 2 bytes min */
         valDer[0] = 0x01;
         valDer[1] = 0x00;
-        EXPECT_TEST(test_SetShortInt_once(0x0100, valDer, 2));
+        EXPECT_TEST(test_GetSetShortInt_once(0x0100, valDer, 2));
 
         /* Input 2 bytes max */
         valDer[0] = 0x00;
         valDer[1] = 0xff;
         valDer[2] = 0xff;
-        EXPECT_TEST(test_SetShortInt_once(0xffff, valDer, 3));
+        EXPECT_TEST(test_GetSetShortInt_once(0xffff, valDer, 3));
 
         /* Input 3 bytes min */
         valDer[0] = 0x01;
         valDer[1] = 0x00;
         valDer[2] = 0x00;
-        EXPECT_TEST(test_SetShortInt_once(0x010000, valDer, 3));
+        EXPECT_TEST(test_GetSetShortInt_once(0x010000, valDer, 3));
 
         /* Input 3 bytes max */
         valDer[0] = 0x00;
         valDer[1] = 0xff;
         valDer[2] = 0xff;
         valDer[3] = 0xff;
-        EXPECT_TEST(test_SetShortInt_once(0xffffff, valDer, 4));
+        EXPECT_TEST(test_GetSetShortInt_once(0xffffff, valDer, 4));
 
         /* Input 4 bytes min */
         valDer[0] = 0x01;
         valDer[1] = 0x00;
         valDer[2] = 0x00;
         valDer[3] = 0x00;
-        EXPECT_TEST(test_SetShortInt_once(0x01000000, valDer, 4));
+        EXPECT_TEST(test_GetSetShortInt_once(0x01000000, valDer, 4));
 
         /* Input 4 bytes max */
         valDer[0] = 0x7f;
         valDer[1] = 0xff;
         valDer[2] = 0xff;
         valDer[3] = 0xff;
-        EXPECT_TEST(test_SetShortInt_once(0x7fffffff, valDer, 4));
+        EXPECT_TEST(test_GetSetShortInt_once(0x7fffffff, valDer, 4));
     }
 
     /* Corner tests for output size */
@@ -124,80 +124,92 @@ int test_SetShortInt(void)
 
         /* Output 1 byte max */
         valDer[0] = 0x7f;
-        EXPECT_TEST(test_SetShortInt_once(0x7f, valDer, 1));
+        EXPECT_TEST(test_GetSetShortInt_once(0x7f, valDer, 1));
 
         /* Output 2 bytes min */
         valDer[0] = 0x00;
         valDer[1] = 0x80;
-        EXPECT_TEST(test_SetShortInt_once(0x80, valDer, 2));
+        EXPECT_TEST(test_GetSetShortInt_once(0x80, valDer, 2));
 
         /* Output 2 bytes max */
         valDer[0] = 0x7f;
         valDer[1] = 0xff;
-        EXPECT_TEST(test_SetShortInt_once(0x7fff, valDer, 2));
+        EXPECT_TEST(test_GetSetShortInt_once(0x7fff, valDer, 2));
 
         /* Output 3 bytes min */
         valDer[0] = 0x00;
         valDer[1] = 0x80;
         valDer[2] = 0x00;
-        EXPECT_TEST(test_SetShortInt_once(0x8000, valDer, 3));
+        EXPECT_TEST(test_GetSetShortInt_once(0x8000, valDer, 3));
 
         /* Output 3 bytes max */
         valDer[0] = 0x7f;
         valDer[1] = 0xff;
         valDer[2] = 0xff;
-        EXPECT_TEST(test_SetShortInt_once(0x7fffff, valDer, 3));
+        EXPECT_TEST(test_GetSetShortInt_once(0x7fffff, valDer, 3));
 
         /* Output 4 bytes min */
         valDer[0] = 0x00;
         valDer[1] = 0x80;
         valDer[2] = 0x00;
         valDer[3] = 0x00;
-        EXPECT_TEST(test_SetShortInt_once(0x800000, valDer, 4));
+        EXPECT_TEST(test_GetSetShortInt_once(0x800000, valDer, 4));
 
         /* Output 4 bytes min */
         valDer[0] = 0x80;
         valDer[1] = 0x00;
         valDer[2] = 0x00;
         valDer[3] = 0x00;
-        EXPECT_TEST(test_SetShortInt_once(0x80000000, valDer, 4));
+        EXPECT_TEST(test_GetSetShortInt_once(0x80000000, valDer, 4));
 
         /* Test negative value encoding. */
         valDer[0] = 0xff;
-        EXPECT_TEST(test_SetShortInt_once(0xffffffff, valDer, 1));
+        EXPECT_TEST(test_GetSetShortInt_once(0xffffffff, valDer, 1));
 
         valDer[0] = 0xff;
         valDer[1] = 0x55;
-        EXPECT_TEST(test_SetShortInt_once(0xffffff55, valDer, 2));
+        EXPECT_TEST(test_GetSetShortInt_once(0xffffff55, valDer, 2));
 
         valDer[0] = 0xff;
         valDer[1] = 0x55;
         valDer[2] = 0x55;
-        EXPECT_TEST(test_SetShortInt_once(0xffff5555, valDer, 3));
+        EXPECT_TEST(test_GetSetShortInt_once(0xffff5555, valDer, 3));
 
         valDer[0] = 0xff;
         valDer[1] = 0x55;
         valDer[2] = 0x55;
         valDer[3] = 0x55;
-        EXPECT_TEST(test_SetShortInt_once(0xff555555, valDer, 4));
+        EXPECT_TEST(test_GetSetShortInt_once(0xff555555, valDer, 4));
 
         valDer[0] = 0x80;
-        EXPECT_TEST(test_SetShortInt_once(0xffffff80, valDer, 1));
+        EXPECT_TEST(test_GetSetShortInt_once(0xffffff80, valDer, 1));
 
         valDer[0] = 0x80;
         valDer[1] = 0x00;
-        EXPECT_TEST(test_SetShortInt_once(0xffff8000, valDer, 2));
+        EXPECT_TEST(test_GetSetShortInt_once(0xffff8000, valDer, 2));
 
         valDer[0] = 0x80;
         valDer[1] = 0x00;
         valDer[2] = 0x00;
-        EXPECT_TEST(test_SetShortInt_once(0xff800000, valDer, 3));
+        EXPECT_TEST(test_GetSetShortInt_once(0xff800000, valDer, 3));
     }
 
     /* Extra tests */
     {
+        word32 inOutIdx = 0U;
+        sword32 decodedVal = 0;
+
         valDer[0] = 0x01;
-        EXPECT_TEST(test_SetShortInt_once(0x01, valDer, 1));
+        EXPECT_TEST(test_GetSetShortInt_once(0x01, valDer, 1));
+
+        valDer[0] = ASN_INTEGER;
+        valDer[1] = 5U;
+        valDer[2] = 0x00U;
+        valDer[3] = 0x80U;
+        valDer[4] = 0x00U;
+        valDer[5] = 0x00U;
+        valDer[6] = 0x00U;
+        ExpectIntEQ(GetShortInt(valDer, &inOutIdx, &decodedVal, 7U), ASN_PARSE_E);
     }
 #endif
 
