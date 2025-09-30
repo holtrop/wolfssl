ce15b27ff6d95daf5dd82d220e0b4c0861c4dc95
Josh Holtrop <josh@wolfssl.com>
2025-09-30 16:38:03 -0400

    Add more RSA APIs

--- wrapper/rust/wolfssl/src/wolfcrypt/rsa.rs
+++ wrapper/rust/wolfssl/src/wolfcrypt/rsa.rs
@@ -18,6 +18,36 @@ pub struct RSA {
 }
 
 impl RSA {
+    pub const HASH_TYPE_NONE       : i32 = ws::wc_HashType_WC_HASH_TYPE_NONE as i32;
+    pub const HASH_TYPE_MD2        : i32 = ws::wc_HashType_WC_HASH_TYPE_MD2 as i32;
+    pub const HASH_TYPE_MD4        : i32 = ws::wc_HashType_WC_HASH_TYPE_MD4 as i32;
+    pub const HASH_TYPE_MD5        : i32 = ws::wc_HashType_WC_HASH_TYPE_MD5 as i32;
+    pub const HASH_TYPE_SHA        : i32 = ws::wc_HashType_WC_HASH_TYPE_SHA as i32;
+    pub const HASH_TYPE_SHA224     : i32 = ws::wc_HashType_WC_HASH_TYPE_SHA224 as i32;
+    pub const HASH_TYPE_SHA256     : i32 = ws::wc_HashType_WC_HASH_TYPE_SHA256 as i32;
+    pub const HASH_TYPE_SHA384     : i32 = ws::wc_HashType_WC_HASH_TYPE_SHA384 as i32;
+    pub const HASH_TYPE_SHA512     : i32 = ws::wc_HashType_WC_HASH_TYPE_SHA512 as i32;
+    pub const HASH_TYPE_MD5_SHA    : i32 = ws::wc_HashType_WC_HASH_TYPE_MD5_SHA as i32;
+    pub const HASH_TYPE_SHA3_224   : i32 = ws::wc_HashType_WC_HASH_TYPE_SHA3_224 as i32;
+    pub const HASH_TYPE_SHA3_256   : i32 = ws::wc_HashType_WC_HASH_TYPE_SHA3_256 as i32;
+    pub const HASH_TYPE_SHA3_384   : i32 = ws::wc_HashType_WC_HASH_TYPE_SHA3_384 as i32;
+    pub const HASH_TYPE_SHA3_512   : i32 = ws::wc_HashType_WC_HASH_TYPE_SHA3_512 as i32;
+    pub const HASH_TYPE_BLAKE2B    : i32 = ws::wc_HashType_WC_HASH_TYPE_BLAKE2B as i32;
+    pub const HASH_TYPE_BLAKE2S    : i32 = ws::wc_HashType_WC_HASH_TYPE_BLAKE2S as i32;
+    pub const HASH_TYPE_SHA512_224 : i32 = ws::wc_HashType_WC_HASH_TYPE_SHA512_224 as i32;
+    pub const HASH_TYPE_SHA512_256 : i32 = ws::wc_HashType_WC_HASH_TYPE_SHA512_256 as i32;
+    pub const HASH_TYPE_SHAKE128   : i32 = ws::wc_HashType_WC_HASH_TYPE_SHAKE128 as i32;
+    pub const HASH_TYPE_SHAKE256   : i32 = ws::wc_HashType_WC_HASH_TYPE_SHAKE256 as i32;
+
+    pub const MGF1NONE       : i32 = ws::WC_MGF1NONE as i32;
+    pub const MGF1SHA1       : i32 = ws::WC_MGF1SHA1 as i32;
+    pub const MGF1SHA224     : i32 = ws::WC_MGF1SHA224 as i32;
+    pub const MGF1SHA256     : i32 = ws::WC_MGF1SHA256 as i32;
+    pub const MGF1SHA384     : i32 = ws::WC_MGF1SHA384 as i32;
+    pub const MGF1SHA512     : i32 = ws::WC_MGF1SHA512 as i32;
+    pub const MGF1SHA512_224 : i32 = ws::WC_MGF1SHA512_224 as i32;
+    pub const MGF1SHA512_256 : i32 = ws::WC_MGF1SHA512_256 as i32;
+
     /// Create a new uninitialized RSA instance.
     ///
     /// This instance will not be holding a valid key.
@@ -157,6 +187,51 @@ impl RSA {
         Ok(dout_size)
     }
 
+    pub fn pss_sign(&mut self, din: &[u8], dout: &mut [u8], hash_algo: u32, mgf: i32, rng: &mut RNG) -> Result<(), i32> {
+        let din_ptr = din.as_ptr() as *const u8;
+        let din_size = din.len() as u32;
+        let dout_ptr = dout.as_ptr() as *mut u8;
+        let dout_size = dout.len() as u32;
+        let rc = unsafe {
+            ws::wc_RsaPSS_Sign(din_ptr, din_size, dout_ptr, dout_size,
+                hash_algo, mgf, &mut self.wc_rsakey, &mut rng.wc_rng)
+        };
+        if rc != 0 {
+            return Err(rc);
+        }
+        Ok(())
+    }
+
+    pub fn pss_verify(&mut self, din: &[u8], dout: &mut [u8], hash_algo: u32, mgf: i32) -> Result<(), i32> {
+        let din_ptr = din.as_ptr() as *const u8;
+        let din_size = din.len() as u32;
+        let dout_ptr = dout.as_ptr() as *mut u8;
+        let dout_size = dout.len() as u32;
+        let rc = unsafe {
+            ws::wc_RsaPSS_Verify(din_ptr, din_size, dout_ptr, dout_size,
+                hash_algo, mgf, &mut self.wc_rsakey)
+        };
+        if rc != 0 {
+            return Err(rc);
+        }
+        Ok(())
+    }
+
+    pub fn pss_check_padding(&mut self, din: &[u8], sig: &mut [u8], hash_algo: u32) -> Result<(), i32> {
+        let din_ptr = din.as_ptr() as *const u8;
+        let din_size = din.len() as u32;
+        let sig_ptr = sig.as_ptr() as *mut u8;
+        let sig_size = sig.len() as u32;
+        let rc = unsafe {
+            ws::wc_RsaPSS_CheckPadding(din_ptr, din_size, sig_ptr, sig_size,
+                hash_algo)
+        };
+        if rc != 0 {
+            return Err(rc);
+        }
+        Ok(())
+    }
+
     pub fn ssl_sign(&mut self, din: &[u8], dout: &mut [u8], rng: &mut RNG) -> Result<(), i32> {
         let din_ptr = din.as_ptr() as *const u8;
         let din_size = din.len() as u32;
@@ -165,6 +240,21 @@ impl RSA {
         let rc = unsafe {
             ws::wc_RsaSSL_Sign(din_ptr, din_size, dout_ptr, dout_size,
                 &mut self.wc_rsakey, &mut rng.wc_rng)
+        };
+        if rc != 0 {
+            return Err(rc);
+        }
+        Ok(())
+    }
+
+    pub fn ssl_verify(&mut self, din: &[u8], dout: &mut [u8]) -> Result<(), i32> {
+        let din_ptr = din.as_ptr() as *const u8;
+        let din_size = din.len() as u32;
+        let dout_ptr = dout.as_ptr() as *mut u8;
+        let dout_size = dout.len() as u32;
+        let rc = unsafe {
+            ws::wc_RsaSSL_Verify(din_ptr, din_size, dout_ptr, dout_size,
+                &mut self.wc_rsakey)
         };
         if rc != 0 {
             return Err(rc);
