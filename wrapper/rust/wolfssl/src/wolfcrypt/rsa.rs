35ee2e1c496704cca5880ca13626d7fef914ecad
Josh Holtrop <josh@wolfssl.com>
2025-09-30 13:16:37 -0400

    Add more RSA Rust APIs

--- wrapper/rust/wolfssl/src/wolfcrypt/rsa.rs
+++ wrapper/rust/wolfssl/src/wolfcrypt/rsa.rs
@@ -103,6 +103,74 @@ impl RSA {
         let rsa = RSA { wc_rsakey };
         Ok(rsa)
     }
+
+    pub fn check(&mut self) -> Result<(), i32> {
+        let rc = unsafe { ws::wc_CheckRsaKey(&mut self.wc_rsakey) };
+        if rc != 0 {
+            return Err(rc);
+        }
+        Ok(())
+    }
+
+    pub fn public_encrypt(&mut self, din: &[u8], dout: &mut [u8], rng: &mut RNG) -> Result<(), i32> {
+        let din_ptr = din.as_ptr() as *const u8;
+        let din_size = din.len() as u32;
+        let dout_ptr = dout.as_ptr() as *mut u8;
+        let dout_size = dout.len() as u32;
+        let rc = unsafe {
+            ws::wc_RsaPublicEncrypt(din_ptr, din_size, dout_ptr, dout_size,
+                &mut self.wc_rsakey, &mut rng.wc_rng)
+        };
+        if rc != 0 {
+            return Err(rc);
+        }
+        Ok(())
+    }
+
+    pub fn private_decrypt(&mut self, din: &[u8], dout: &mut [u8]) -> Result<(), i32> {
+        let din_ptr = din.as_ptr() as *const u8;
+        let din_size = din.len() as u32;
+        let dout_ptr = dout.as_ptr() as *mut u8;
+        let dout_size = dout.len() as u32;
+        let rc = unsafe {
+            ws::wc_RsaPrivateDecrypt(din_ptr, din_size, dout_ptr, dout_size,
+                &mut self.wc_rsakey)
+        };
+        if rc != 0 {
+            return Err(rc);
+        }
+        Ok(())
+    }
+
+    pub fn rsa_direct(&mut self, din: &[u8], dout: &mut [u8], typ: i32, rng: &mut RNG) -> Result<u32, i32> {
+        let din_ptr = din.as_ptr() as *const u8;
+        let din_size = din.len() as u32;
+        let dout_ptr = dout.as_ptr() as *mut u8;
+        let mut dout_size = dout.len() as u32;
+        let rc = unsafe {
+            ws::wc_RsaDirect(din_ptr, din_size, dout_ptr, &mut dout_size,
+                &mut self.wc_rsakey, typ, &mut rng.wc_rng)
+        };
+        if rc != 0 {
+            return Err(rc);
+        }
+        Ok(dout_size)
+    }
+
+    pub fn ssl_sign(&mut self, din: &[u8], dout: &mut [u8], rng: &mut RNG) -> Result<(), i32> {
+        let din_ptr = din.as_ptr() as *const u8;
+        let din_size = din.len() as u32;
+        let dout_ptr = dout.as_ptr() as *mut u8;
+        let dout_size = dout.len() as u32;
+        let rc = unsafe {
+            ws::wc_RsaSSL_Sign(din_ptr, din_size, dout_ptr, dout_size,
+                &mut self.wc_rsakey, &mut rng.wc_rng)
+        };
+        if rc != 0 {
+            return Err(rc);
+        }
+        Ok(())
+    }
 }
 
 impl Drop for RSA {
