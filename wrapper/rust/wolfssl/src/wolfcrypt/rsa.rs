b165701d92266a04cd505bc66a663eadfeab4387
Josh Holtrop <josh@wolfssl.com>
2025-10-02 15:05:59 -0400

    Test export_key()

--- wrapper/rust/wolfssl/src/wolfcrypt/rsa.rs
+++ wrapper/rust/wolfssl/src/wolfcrypt/rsa.rs
@@ -223,21 +223,6 @@ impl RSA {
         Ok(())
     }
 
-    pub fn rsa_direct(&mut self, din: &[u8], dout: &mut [u8], typ: i32, rng: &mut RNG) -> Result<u32, i32> {
-        let din_ptr = din.as_ptr() as *const u8;
-        let din_size = din.len() as u32;
-        let dout_ptr = dout.as_ptr() as *mut u8;
-        let mut dout_size = dout.len() as u32;
-        let rc = unsafe {
-            ws::wc_RsaDirect(din_ptr, din_size, dout_ptr, &mut dout_size,
-                &mut self.wc_rsakey, typ, &mut rng.wc_rng)
-        };
-        if rc != 0 {
-            return Err(rc);
-        }
-        Ok(dout_size)
-    }
-
     pub fn pss_sign(&mut self, din: &[u8], dout: &mut [u8], hash_algo: u32, mgf: i32, rng: &mut RNG) -> Result<i32, i32> {
         let din_ptr = din.as_ptr() as *const u8;
         let din_size = din.len() as u32;
@@ -298,6 +283,31 @@ impl RSA {
             return Err(rc);
         }
         Ok(rc)
+    }
+
+    pub fn rsa_direct(&mut self, din: &[u8], dout: &mut [u8], typ: i32, rng: &mut RNG) -> Result<u32, i32> {
+        let din_ptr = din.as_ptr() as *const u8;
+        let din_size = din.len() as u32;
+        let dout_ptr = dout.as_ptr() as *mut u8;
+        let mut dout_size = dout.len() as u32;
+        let rc = unsafe {
+            ws::wc_RsaDirect(din_ptr, din_size, dout_ptr, &mut dout_size,
+                &mut self.wc_rsakey, typ, &mut rng.wc_rng)
+        };
+        if rc != 0 {
+            return Err(rc);
+        }
+        Ok(dout_size)
+    }
+
+    pub fn set_rng(&mut self, rng: &mut RNG) -> Result<(), i32> {
+        let rc = unsafe {
+            ws::wc_RsaSetRNG(&mut self.wc_rsakey, &mut rng.wc_rng)
+        };
+        if rc != 0 {
+            return Err(rc);
+        }
+        Ok(())
     }
 
     pub fn ssl_sign(&mut self, din: &[u8], dout: &mut [u8], rng: &mut RNG) -> Result<i32, i32> {
--- wrapper/rust/wolfssl/tests/test_rsa.rs
+++ wrapper/rust/wolfssl/tests/test_rsa.rs
@@ -8,4 +8,26 @@ fn test_rsa_generate() {
     rsa.check().expect("Error with check()");
     let encrypt_size = rsa.get_encrypt_size().expect("Error with get_encrypt_size()");
     assert_eq!(encrypt_size, 256);
+    let mut e: [u8; 256] = [0; 256];
+    let mut e_size: u32 = 0;
+    let mut n: [u8; 256] = [0; 256];
+    let mut n_size: u32 = 0;
+    let mut d: [u8; 256] = [0; 256];
+    let mut d_size: u32 = 0;
+    let mut p: [u8; 256] = [0; 256];
+    let mut p_size: u32 = 0;
+    let mut q: [u8; 256] = [0; 256];
+    let mut q_size: u32 = 0;
+    rsa.export_key(&mut e, &mut e_size, &mut n, &mut n_size,
+        &mut d, &mut d_size, &mut p, &mut p_size, &mut q, &mut q_size).expect("Error with export_key()");
+    assert_ne!(e, [0; 256]);
+    assert!(e_size > 0);
+    assert_ne!(n, [0; 256]);
+    assert!(n_size > 0);
+    assert_ne!(d, [0; 256]);
+    assert!(d_size > 0);
+    assert_ne!(p, [0; 256]);
+    assert!(p_size > 0);
+    assert_ne!(q, [0; 256]);
+    assert!(q_size > 0);
 }
