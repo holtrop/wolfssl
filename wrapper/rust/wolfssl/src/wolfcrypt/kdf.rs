17d1b904f67081c537f759b4a1f47b0b4bdd61ec
Josh Holtrop <josh@wolfssl.com>
2025-11-06 16:45:25 -0500

    Update kdf

--- wrapper/rust/wolfssl/src/wolfcrypt/kdf.rs
+++ wrapper/rust/wolfssl/src/wolfcrypt/kdf.rs
@@ -317,6 +317,71 @@ pub fn tls13_hkdf_extract(typ: i32, salt: Option<&[u8]>, key: Option<&mut [u8]>,
     Ok(())
 }
 
+/// Perform RFC 5869 HKDF-Extract operation for TLS v1.3 key derivation (with
+/// optional heap and device ID).
+///
+/// # Parameters
+///
+/// * `typ`: Hash type, one of `HMAC::TYPE_*`.
+/// * `salt`: Optional Salt value.
+/// * `key`: Optional Initial Key Material (IKM).
+/// * `out`: Output buffer to store TLS1.3 HKDF-Extract result (generated
+///   Pseudo-Random Key (PRK)). The size of this buffer must match
+///   `HMAC::get_hmac_size_by_type(typ)`.
+/// * `heap`: Optional heap hint.
+/// * `dev_id` Optional device ID to use with crypto callbacks or async hardware.
+///
+/// # Returns
+///
+/// Returns either Ok(()) on success or Err(e) containing the wolfSSL
+/// library error code value.
+///
+/// # Example
+///
+/// ```rust
+/// use wolfssl::wolfcrypt::hmac::HMAC;
+/// use wolfssl::wolfcrypt::kdf::*;
+/// use wolfssl::wolfcrypt::sha::SHA256;
+/// let mut secret = [0u8; SHA256::DIGEST_SIZE];
+/// tls13_hkdf_extract_ex(HMAC::TYPE_SHA256, None, None, &mut secret, None, None).expect("Error with tls13_hkdf_extract_ex()");
+/// ```
+pub fn tls13_hkdf_extract_ex(typ: i32, salt: Option<&[u8]>, key: Option<&mut [u8]>, out: &mut [u8], heap: Option<*mut std::os::raw::c_void>, dev_id: Option<i32>) -> Result<(), i32> {
+    let mut salt_ptr = core::ptr::null();
+    let mut salt_size = 0u32;
+    if let Some(salt) = salt {
+        salt_ptr = salt.as_ptr();
+        salt_size = salt.len() as u32;
+    }
+    let mut ikm_buf = [0u8; ws::WC_MAX_DIGEST_SIZE as usize];
+    let mut ikm_ptr = ikm_buf.as_mut_ptr();
+    let mut ikm_size = 0u32;
+    if let Some(key) = key {
+        if key.len() > 0 {
+            ikm_ptr = key.as_mut_ptr();
+            ikm_size = key.len() as u32;
+        }
+    }
+    if out.len() != HMAC::get_hmac_size_by_type(typ)? {
+        return Err(ws::wolfCrypt_ErrorCodes_BUFFER_E);
+    }
+    let heap = match heap {
+        Some(heap) => heap,
+        None => core::ptr::null_mut(),
+    };
+    let dev_id = match dev_id {
+        Some(dev_id) => dev_id,
+        None => ws::INVALID_DEVID,
+    };
+    let rc = unsafe {
+        ws::wc_Tls13_HKDF_Extract_ex(out.as_mut_ptr(), salt_ptr, salt_size,
+            ikm_ptr, ikm_size, typ, heap, dev_id)
+    };
+    if rc != 0 {
+        return Err(rc);
+    }
+    Ok(())
+}
+
 /// Perform RFC 5869 HKDF-Expand operation for TLS v1.3 key derivation.
 ///
 /// This utilizes HMAC to convert `key`, `label`, and `info` into a
@@ -373,6 +438,81 @@ pub fn tls13_hkdf_expand_label(typ: i32, key: &[u8], protocol: &[u8], label: &[u
         ws::wc_Tls13_HKDF_Expand_Label(out.as_mut_ptr(), out_size,
             key.as_ptr(), key_size, protocol.as_ptr(), protocol_size,
             label.as_ptr(), label_size, info.as_ptr(), info_size, typ)
+    };
+    if rc != 0 {
+        return Err(rc);
+    }
+    Ok(())
+}
+
+/// Perform RFC 5869 HKDF-Expand operation for TLS v1.3 key derivation (with
+/// optional heap and device ID).
+///
+/// This utilizes HMAC to convert `key`, `label`, and `info` into a
+/// derived key which is written to `out`.
+///
+/// # Parameters
+///
+/// * `typ`: Hash type, one of `HMAC::TYPE_*`.
+/// * `key`: Key to use for KDF (typically output of `tls13_hkdf_extract()`).
+/// * `protocol`: Buffer containing TLS protocol.
+/// * `label`: Buffer containing label.
+/// * `info`: Buffer containing additional info.
+/// * `out`: Output buffer to store TLS1.3 HKDF-Expand result. The buffer can be
+///   any size.
+/// * `heap`: Optional heap hint.
+/// * `dev_id` Optional device ID to use with crypto callbacks or async hardware.
+///
+/// # Returns
+///
+/// Returns either Ok(()) on success or Err(e) containing the wolfSSL
+/// library error code value.
+///
+/// # Example
+///
+/// ```rust
+/// use wolfssl::wolfcrypt::hmac::HMAC;
+/// use wolfssl::wolfcrypt::kdf::*;
+/// use wolfssl::wolfcrypt::sha::SHA256;
+/// let hash_hello1 = [
+///     0x63u8, 0x83, 0x58, 0xab, 0x36, 0xcd, 0x0c, 0xf3,
+///     0x26, 0x07, 0xb5, 0x5f, 0x0b, 0x8b, 0x45, 0xd6,
+///     0x7d, 0x5b, 0x42, 0xdc, 0xa8, 0xaa, 0x06, 0xfb,
+///     0x20, 0xa5, 0xbb, 0x85, 0xdb, 0x54, 0xd8, 0x8b
+/// ];
+/// let client_early_traffic_secret = [
+///     0x20u8, 0x18, 0x72, 0x7c, 0xde, 0x3a, 0x85, 0x17, 0x72, 0xdc, 0xd7, 0x72,
+///     0xb0, 0xfc, 0x45, 0xd0, 0x62, 0xb9, 0xbb, 0x38, 0x69, 0x05, 0x7b, 0xb4,
+///     0x5e, 0x58, 0x5d, 0xed, 0xcd, 0x0b, 0x96, 0xd3
+/// ];
+/// let mut secret = [0u8; SHA256::DIGEST_SIZE];
+/// tls13_hkdf_extract(HMAC::TYPE_SHA256, None, None, &mut secret).expect("Error with tls13_hkdf_extract()");
+/// let protocol_label = b"tls13 ";
+/// let ce_traffic_label = b"c e traffic";
+/// let mut expand_out = [0u8; SHA256::DIGEST_SIZE];
+/// tls13_hkdf_expand_label_ex(HMAC::TYPE_SHA256, &secret,
+///     protocol_label, ce_traffic_label,
+///     &hash_hello1, &mut expand_out, None, None).expect("Error with tls13_hkdf_expand_label_ex()");
+/// ```
+pub fn tls13_hkdf_expand_label_ex(typ: i32, key: &[u8], protocol: &[u8], label: &[u8], info: &[u8], out: &mut [u8], heap: Option<*mut std::os::raw::c_void>, dev_id: Option<i32>) -> Result<(), i32> {
+    let key_size = key.len() as u32;
+    let protocol_size = protocol.len() as u32;
+    let label_size = label.len() as u32;
+    let info_size = info.len() as u32;
+    let out_size = out.len() as u32;
+    let heap = match heap {
+        Some(heap) => heap,
+        None => core::ptr::null_mut(),
+    };
+    let dev_id = match dev_id {
+        Some(dev_id) => dev_id,
+        None => ws::INVALID_DEVID,
+    };
+    let rc = unsafe {
+        ws::wc_Tls13_HKDF_Expand_Label_ex(out.as_mut_ptr(), out_size,
+            key.as_ptr(), key_size, protocol.as_ptr(), protocol_size,
+            label.as_ptr(), label_size, info.as_ptr(), info_size, typ,
+            heap, dev_id)
     };
     if rc != 0 {
         return Err(rc);
